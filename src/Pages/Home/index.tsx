701d48bc9cd147d2196cc87372f2911bd623e65b
Nam Ho Van <vannam272008@gmail.com>
2023-07-17 14:31:10 +0700

    fix create Request form data

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,20 +1,18 @@
-import { useEffect, useState } from "react";
-import request from "../../Utils/request";
 
 
 
 // Exemple Get data using request (axios)
 const Home = () => {
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(10);
-  useEffect(() => {
-    const endpoint = "/request/get-all?page=" + page + "&limit=" + limit;
-    const response = request.get(endpoint).then((res) => {
-      console.log(res.data);
-    }
-    );
+  // const [page, setPage] = useState(1);
+  // const [limit, setLimit] = useState(10);
+  // useEffect(() => {
+  //   const endpoint = "/request/get-all?page=" + page + "&limit=" + limit;
+  //   const response = request.get(endpoint).then((res) => {
+  //     console.log(res.data);
+  //   }
+  //   );
 
-  }, [page, limit])
+  // }, [page, limit])
   return <>Home</>
 }
 
