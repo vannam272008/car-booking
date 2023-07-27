277e748fe85a52fd498f111f4f9a4b386e2c10f0
Nguyen Thanh Sang <60134662+chivas10111@users.noreply.github.com>
2023-07-27 10:14:43 +0700

    update route

---
+++ src/routes/routes.js
@@ -0,0 +1,71 @@
+import AddRequest from "../Components/AddRequest/AddRequest/addRequest";
+import AdminPage from "../Components/AdminPage";
+import DetailRequest from "../Components/DetailRequest/DetailRequest/detailRequest";
+import EditRequest from "../Components/EditRequest/EditRequest/editRequest";
+import ContentStructure from "../Components/OrganizationalStructure/ContentStructure/contentStructure";
+import Profile from "../Components/Profile";
+import PageNotFound from "../Pages/404";
+import Login from "../Pages/Login/Login";
+import ManageRequest from "../Pages/ManageRequest/ManageRequest";
+import Register from "../Pages/Register/Register";
+import SettingPage from "../Pages/Setting/settingPage";
+
+const public_routes = [
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+];
+
+const private_routes = [
+  {
+    path: "/request/carbooking",
+    element: <ManageRequest />,
+  },
+  {
+    path: "/",
+    element: <ManageRequest />,
+  },
+  {
+    path: "/request/carbooking/edit/:requestId",
+    element: <EditRequest />,
+  },
+  {
+    path: "/request/carbooking/detail/:requestId",
+    element: <DetailRequest />,
+  },
+  {
+    path: "/request/addrequest",
+    element: <AddRequest />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/setting",
+    element: <SettingPage />,
+  },
+  {
+    path: "/setting/structure",
+    element: <ContentStructure />,
+  },
+  {
+    path: "/setting/profile/:userID",
+    element: <Profile />,
+  },
+  {
+    path: "/setting/admin",
+    element: <AdminPage />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+];
+
+export { public_routes, private_routes };
